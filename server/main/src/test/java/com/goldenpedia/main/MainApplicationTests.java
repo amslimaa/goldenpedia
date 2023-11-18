b6397cf10779599771dd0abe79b0d4a1a8bec5a2
Amos Lima <social.amos@gmail.com>
2023-11-18 20:42:35 -0300

    Deploy on render

--- server/main/src/test/java/com/goldenpedia/main/MainApplicationTests.java
+++ server/main/src/test/java/com/goldenpedia/main/MainApplicationTests.java
@@ -1,13 +1,9 @@
 package com.goldenpedia.main;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
